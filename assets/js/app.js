69f69061060ead67456716536aa797c4879f41fa
Iryna Aleksandrova <110558905+IreneAleks@users.noreply.github.com>
2022-11-08 16:45:15 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,21 @@
 $(function() {
 
+    // Button scroll
+
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+        anchor.addEventListener("click", function(event) {
+            event.preventDefault();
+            const blockID = anchor.getAttribute('href')
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            })
+        })
+    }
+
+
     // Modal Window
 
     const modalCall = $("[data-modal]");
